ec7786f26d6fb0399b042d14ddb4c9b430c3df57
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-02-08 23:38:09 +0000

    No sentry

--- ingester/__init__.py
+++ ingester/__init__.py
@@ -5,14 +5,14 @@ import atexit
 import sentry_sdk
 
 if __name__ == "__main__":
-    sentry_sdk.init(
-        dsn="https://afd79a357e174a3aa35ba89c3d4d4611@sentry.housestats.co.uk/2",
+    # sentry_sdk.init(
+    #     dsn="https://afd79a357e174a3aa35ba89c3d4d4611@sentry.housestats.co.uk/2",
 
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0
-    )
+    #     # Set traces_sample_rate to 1.0 to capture 100%
+    #     # of transactions for performance monitoring.
+    #     # We recommend adjusting this value in production.
+    #     traces_sample_rate=1.0
+    # )
 
     ingester = Ingest()
     atexit.register(asyncio.run, ingester.remove_status())
